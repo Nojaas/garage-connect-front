c5f4062911bf07df342eed8700ee51e20afc2927
Nojaas <jasonleroyle91@gmail.com>
2024-10-30 10:56:51 +0100

    remove firebase messaging

--- utils/firebase.tsx
+++ utils/firebase.tsx
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -19,6 +18,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
 const storage = getStorage(app);
-const messaging = getMessaging(app);
 
-export { auth, db, storage, app, functions, messaging };
+export { auth, db, storage, app, functions };
